3e7555877a32321d98466eb1f8120b47cbafdee2
Carter Andrew <carterinmotion@gmail.com>
2024-09-19 17:17:01 -0600

    Updates

---
+++ assets/nyagi-BSbdp6N1.js
@@ -0,0 +1,8 @@
+import{j as n}from"./index-Cfb5PDhl.js";function e(r){const s={br:"br",h1:"h1",img:"img",p:"p",strong:"strong",...r.components};return n.jsxs(n.Fragment,{children:[n.jsx(s.h1,{id:"nyagi",children:"NYAGI"}),`
+`,n.jsx(s.p,{children:n.jsx(s.img,{src:"https://nyagi.org",alt:"logo"})}),`
+`,n.jsxs(s.p,{children:[n.jsx(s.strong,{children:"Description"}),n.jsx(s.br,{}),`
+`,"A non-profit helping save mothers lives I've been"]}),`
+`,n.jsxs(s.p,{children:[n.jsx(s.strong,{children:"Year"}),n.jsx(s.br,{}),`
+`,"2024"]}),`
+`,n.jsxs(s.p,{children:[n.jsx(s.strong,{children:"Runtime"}),n.jsx(s.br,{}),`
+`,"KDE Plasma 6"]})]})}function i(r={}){const{wrapper:s}=r.components||{};return s?n.jsx(s,{...r,children:n.jsx(e,{...r})}):e(r)}export{i as default};
---
+++ assets/somaliland-BDdr8Zb5.js
@@ -0,0 +1,10 @@
+import{j as n}from"./index-Cfb5PDhl.js";function s(o){const r={br:"br",h1:"h1",img:"img",p:"p",strong:"strong",...o.components};return n.jsxs(n.Fragment,{children:[n.jsx(r.h1,{id:"nyagi-somaliland-project",children:"NYAGI Somaliland Project"}),`
+`,n.jsx(r.p,{children:n.jsx(r.img,{src:"https://nyagi.org",alt:"logo"})}),`
+`,n.jsxs(r.p,{children:[n.jsx(r.strong,{children:"Description"}),n.jsx(r.br,{}),`
+`,"An expidition to the Republic of Somaliland to train doctors in ultrasound for prenadal care"]}),`
+`,n.jsxs(r.p,{children:[n.jsx(r.strong,{children:"Year"}),n.jsx(r.br,{}),`
+`,"2024"]}),`
+`,n.jsxs(r.p,{children:[n.jsx(r.strong,{children:"Project Memebers"}),n.jsx(r.br,{}),`
+`,"Cliff Gronseth, Edna Adan, Amal Adan"]}),`
+`,n.jsxs(r.p,{children:[n.jsx(r.strong,{children:"Project Sponsor"}),`
+doTERRA`]})]})}function e(o={}){const{wrapper:r}=o.components||{};return r?n.jsx(r,{...o,children:n.jsx(s,{...o})}):s(o)}export{e as default};
